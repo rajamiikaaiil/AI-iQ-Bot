1cb488b052ef939d8e9a772840356e45d01bc6ab
SuGit-Demo <89434151+SuGit-Demo@users.noreply.github.com>
2022-01-25 15:47:47 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import render_template
+
+@app.route('/')
+@app.route('/printMessage')
+def printMessage():
+  text = '**Message from app.py**'
+  return render_template('index.html',message=text)
