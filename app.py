d1a5286ff5a8971c6f75eb93e9a05b66aa75cbbd
SuGit-Demo <89434151+SuGit-Demo@users.noreply.github.com>
2022-01-25 15:59:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import render_template
+from flask import Flask, render_template
 
-#@app.route('/')
+@app.route('/')
 @app.route('/printMessage')
 def printMessage():
   text = '**Message from app.py**'
   return render_template('index.html',message=text)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
