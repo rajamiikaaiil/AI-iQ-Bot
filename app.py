b3fdae910056733f699e6944b27cd471f26f389d
SuGit-Demo <89434151+SuGit-Demo@users.noreply.github.com>
2022-01-26 09:00:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 
-pheu = Flask(__name__)
+app = Flask(__name__)
 
-@pheu.route("/")
-@pheu.route("/printMessage")
+@app.route("/")
+@app.route("/printMessage")
 def printMessage():
   text = "**Message from app.py**"
   return render_template('index.html',message=text)
