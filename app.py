1d51968d63a9711188c6eabff4aacf1a74ce5647
SuGit-Demo <89434151+SuGit-Demo@users.noreply.github.com>
2022-01-26 10:14:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,11 @@ app = Flask(__name__)
 #app.route("/") is call to the root URL and will execute the codes in the 'home' view function
 @app.route("/")
 def home():
-  #
+  #Redirect(url_for ......) is to redirect the user to the path specified in this case /printMessage and pass the variable 'message' over
   return redirect(url_for('printMessage', message='Hello its me!!'))
 
-#Call to the /printMessage URL and will execute the codes in the 'printMessage' view function
+#Call to the /printMessage URL and will execute the codes in the 'printMessage' view function which will accept an argument 'message'
 @app.route("/printMessage/<message>")
 def printMessage(message):
+  #return render_template will call an HTML page to the user in this case 'index.html' which by default located in the 'templates' directory
   return render_template('index.html',message=message)
